1661ae655e3a5195d5c4768889ad0b93c284d3fa
Callum Daniel <callum.daniel17@gmail.com>
2025-06-05 16:02:23 +1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'C:\\xampp\\htdocs\\test'
-  },
   plugins: [react()],
   server: {
     host: '127.0.0.1',
